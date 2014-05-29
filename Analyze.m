bb411a1dca8b4823fe92dd81ff642f8391df39f0
Oleg Komarov <o.komarov11@imperial.ac.uk>
2014-05-29 18:19:20 +0100

    Analyze, added sampling routine
    Signed-off-by: Oleg Komarov <o.komarov11@imperial.ac.uk>

--- Analyze.m
+++ Analyze.m
@@ -38,7 +38,7 @@ fhandle = str2func(fun);
 try
     tic
     d   = '.\data\TAQ';
-    dd  = dir(fullfile(d,'*.mat'));
+    dd  = dir(fullfile(d,'T*.mat'));
     N   = numel(dd);
     res = deal(cell(N,1));
     
@@ -55,7 +55,7 @@ try
         % Load data
         s      = load(fullfile(d,dd(f).name));
         % Apply function
-        res{f} = fhandle(s, cached{f});
+        res{f} = fhandle(s, [cached(f), f]);
     end
     % Collect all results and convert to dataset
     res = cat(1,res{:});
@@ -110,7 +110,7 @@ res = [accumarray(subs, ret,[nmst,1],@min,fill), accumarray(subs, ret,[nmst,1],@
 end
 %% Count bad prices
 function res = badprices(s, cached)
-
+cached = cached{1};
 % Flag bad days if number of intraday bad prices is > dailycut
 dailycut = .5;
 
@@ -129,6 +129,8 @@ res = accumarray(RunLength((1:size(s.mst,1))',nobs), inan | ibadprice) > ceil(da
 end
 %% Check stats for returns
 function res = avgtimestep(s,cached)
+cached = cached{1};
+
 % Number of observations per day
 nobs = double(s.mst.To - s.mst.From + 1);
 nmst = size(s.mst,1);
@@ -141,20 +143,22 @@ ibadprice     = ibadprice ~= 1;
 inan          = inan | ibadprice;
 
 % STEP 3) Take median price if same timestamp
-mstrow         = RunLength((1:nmst)',nobs);
-mstrow         = mstrow(~inan);
-[times,~,subs] = unique(mstrow + hhmmssmat2serial(s.data.Time(~inan,:)));
-price          = accumarray(subs, s.data.Price(~inan),[],@fast_median);
+mstrow = RunLength((1:nmst)',nobs);
+mstrow = mstrow(~inan);
+times  = single(unique(mstrow + hhmmssmat2serial(s.data.Time(~inan,:))));
 
 % Calculate average intraday unique time step after selection, bad prices and consolidation
-subs      = single(fix(times));
-n         = accumarray(subs, single(1), [nmst,1],   [], ones('single'));
-openTime  = accumarray(subs,     times, [nmst,1], @min, ones('single'));
-closeTime = accumarray(subs,     times, [nmst,1], @max, ones('single'));
+subs      = uint32(fix(times));
+l         = ones('single');
+n         = accumarray(subs,      l, [nmst,1],   [], l);
+openTime  = accumarray(subs,  times, [nmst,1], @min, l);
+closeTime = accumarray(subs,  times, [nmst,1], @max, l);
 res       = (closeTime - openTime)./(n-1);
 end
 %% Check how many bad prices
 function res = cleanprices(s,cached)
+cached = cached{1};
+
 % Number of observations per day
 nobs = double(s.mst.To - s.mst.From + 1);
 
@@ -190,6 +194,54 @@ if ~all(inan)
     [res(1),res(2)] = MinMaxElem(ret);
 end
 fprintf('\t\t\t\t\t\t\t %.1f - %.1f\n',selper,badserper)
+end
+%% Sampling
+function res = sample(s,cached)
+nfile  = cached{end};
+cached = cached{1};
+% Number of observations per day
+nobs   = double(s.mst.To - s.mst.From + 1);
+nmst   = size(s.mst,1);
+
+% STEP 1) Selection
+inan = selecttrades(s.data);
+
+% STEP 2) Bad prices prices are 1.5x or .65x the daily median (net of selection nans)
+[~,iprice] = histc(s.data.Price./RunLength(cached.MedPrice,nobs), [.65,1.51]);
+iprice     = iprice ~= 1;
+inan       = inan | iprice;
+
+% STEP 3) Bad series/days
+inan = inan | RunLength(cached.Baddays,nobs);
+
+% STEP 4) Filter out days with < 30min avg timestep or securities with 50% fewtrades days
+inan = inan | RunLength(cached.Timestep,nobs);
+
+% STEP 5) Clean prices
+s.data.Price(inan) = NaN;
+
+if ~all(inan)
+
+    % STEP 6) Median prices for same timestamps
+    mstrow           = RunLength((1:nmst)',nobs);
+    mstrow           = mstrow(~inan);
+    [unTimes,~,subs] = unique(mstrow + hhmmssmat2serial(s.data.Time(~inan,:)));
+    price            = accumarray(subs, s.data.Price(~inan),[],@fast_median);
+        
+    % STEP 7) Sample on fixed grid (easier to match sp500) 
+    grid  = (9.5/24:5/(60*24):16/24)';
+    price = fixedsampling(double([unTimes, price]), 'Previous', grid);
+    idx   = fix(price(:,1));
+    dates = yyyymmdd2serial(double(s.mst.Date(idx))) + rem(price(:,1),1);
+    
+    % Save
+    res = [];
+    fname = fullfile('.\data\TAQ\sampled',sprintf('S5m_%04d.mat',nfile));
+    data = dataset({cached.UnID(idx),'UnID'}, {dates,'Datetime'},{price(:,2),'Price'});
+    save(fname, 'data')
+end    
+        
+
 end
 %% Betas
 function res = betas(s,cached)
