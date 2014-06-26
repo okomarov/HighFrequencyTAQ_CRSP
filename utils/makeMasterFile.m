6c47820b5cd92586c32fca3ff66b5c52b8d3bb70
oleg komarov <o.komarov11@imperial.ac.uk>
2014-06-26 19:43:14 +0100

    [Fix] makeMasterFile: fix the shifts in id for each block
    Signed-off-by: oleg komarov <o.komarov11@imperial.ac.uk>

--- utils/makeMasterFile.m
+++ utils/makeMasterFile.m
@@ -1,5 +1,7 @@
-function makeMasterFile(path2matfiles)
+function makeMasterFile(path2matfiles, isdebug)
 % MAKEMASTERFILE Groups all master records from single *.mat files into one master table
+
+if nargin < 2 || isempty(isdebug), isdebug = false; end
 
 mstname = 'mst';
 idsname = 'ids';
@@ -7,8 +9,8 @@ idsname = 'ids';
 d = dir(fullfile(path2matfiles,'*.mat'));
 
 % Open matlabpool
-if matlabpool('size') == 0
-    matlabpool('open', 4, 'AttachedFiles',{'.\utils\poolStartup.m'})
+if isempty(gcp('nocreate')) && ~isdebug
+    parpool(4, 'AttachedFiles',{'.\utils\poolStartup.m'})
 end
 
 % Preallocate
@@ -24,24 +26,29 @@ parfor f = 1:nfiles
     mst{f}.Id  = int64(mst{f}.Id);
     ids{f}     = s.(idsname);
 end
-matlabpool close
+delete(gcp)
 toc
 
 % Number of rows per block
 lenmst = cellfun(@(x) size(x,1),mst);
 
-% Add number of file as 4th column to mst{2} and cat all mst
-mst = arrayfun(@(x) [mst{x} dataset({repmat(x,lenmst(x),1),'File'})],uint16((1:nfiles))','un',0);
+% Shift in Ids from a block to another, i.e. not all ids are in mst.Id
+lenids    = cellfun(@(x) size(x,1),ids);
+maxids    = cellfun(@(x) max(x.Id),mst);
+endshifts = cast(lenids,'like',maxids) - maxids + 1;
+
+% Concatenate all
+fprintf('Concatenating records.\n')
 ids = cat(1,ids{:});
-fprintf('Concatenating master records.\n')
-tic
 mst = cat(1,mst{:}); 
-toc
+
+% Add number of file to mst
+mst.File(:,1) = RunLength(uint16(1:nfiles), lenmst);
 
 % Re-index id to whole block
 cumpos           = cumsum(lenmst(1:end-1));
 mst.Id           = [1; diff(mst.Id)];
-mst.Id(cumpos+1) = 1;
+mst.Id(cumpos+1) = endshifts(1:end-1);
 mst.Id           = cumsum(mst.Id);
 
 % Make unique tickers
