c3a4a4578ff8bd42d07d780e3fdc622235abfb06
oleg komarov <o.komarov11@imperial.ac.uk>
2014-06-27 14:55:51 +0100

    [Fix] loadresults: concatenation bug
    Signed-off-by: oleg komarov <o.komarov11@imperial.ac.uk>

--- utils/loadresults.m
+++ utils/loadresults.m
@@ -2,7 +2,7 @@ function loadresults(name,outname)
 if nargin == 1, outname = name; end
 resdir  = '.\results';
 files   = dir(fullfile(resdir, sprintf('*%s.mat', name)));
-[~,idx] = max(files.datenum); % Most recent
+[~,idx] = max([files.datenum]); % Most recent
 s       = load(fullfile(resdir,files(idx).name));
 assignin('caller', outname, s.(char(fieldnames(s))));
 end
