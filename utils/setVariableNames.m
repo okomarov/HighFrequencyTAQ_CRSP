c6428e3c3430a9487af72272a99c05b01158623e
Oleg Komarov <o.komarov11@imperial.ac.uk>
2014-09-30 19:12:17 +0100

    [Fix] setVariableNames: error if first input not table/dataset
    Signed-off-by: Oleg Komarov <o.komarov11@imperial.ac.uk>

--- utils/setVariableNames.m
+++ utils/setVariableNames.m
@@ -6,5 +6,7 @@ if isa(A,'dataset')
     A.Properties.VarNames = vnames;
 elseif istable(A)
     A.Properties.VariableNames = vnames;
+else
+    error('setVariableNames:invalidType','A should be a dataset or a table.')
 end
 end
