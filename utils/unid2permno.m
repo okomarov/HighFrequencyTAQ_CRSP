97511c3bc66a5d4a01fc4fe0d542473eb310fc27
Oleg Komarov <o.komarov11@imperial.ac.uk>
2014-09-30 19:13:48 +0100

    [Fix] unid2permno: allow unmatched UnID
    Signed-off-by: Oleg Komarov <o.komarov11@imperial.ac.uk>

--- utils/unid2permno.m
+++ utils/unid2permno.m
@@ -6,12 +6,17 @@ function permnos = unid2permno(unids)
 %
 %   PERMNOS = UNID2PERMNO(...) Array of PERMNOS in preserved order.
 %
-% Note: only one permno per UnID (excluding NaN permnos).
+% Note: only one permno per UnID (excluding NaN permnos). Also, some
+%       UnID might be 65335, i.e. intmax('uint16').
 
+% Preallocate
+permnos      = NaN(size(unids));
+% Load mapping
+taq2crsp     = loadresults('taq2crsp');
+taq2crsp     = unique(taq2crsp(:,{'permno','ID'}));
+% Map
+[idx,pos]    = ismember(unids, taq2crsp.ID);
+permnos(idx) = taq2crsp.permno(pos(idx));
 
-taq2crsp = loadresults('taq2crsp');
-taq2crsp = unique(taq2crsp(:,{'permno','ID'}));
-[~,pos]  = ismember(unids, taq2crsp.ID);
-permnos  = taq2crsp.permno(pos);
 
 end
