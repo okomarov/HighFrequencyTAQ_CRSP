dd64a0e0cec6efc933da4bfe8a2efd2e7b885d1a
Oleg Komarov <o.komarov11@imperial.ac.uk>
2014-09-30 19:10:03 +0100

    [Fix/Change] importDfequery: proper handling of missing codes (from sas)
    Signed-off-by: Oleg Komarov <o.komarov11@imperial.ac.uk>

--- utils/importDfequery.m
+++ utils/importDfequery.m
@@ -2,17 +2,18 @@ function dfequery = importDfequery(path2zip)
 % IMPORTDFEQUERY Imports the zipped CRSP dfequery dataset into a table
 %
 %   IMPORTDFEQUERY (PATH2ZIP) 
+%
+% Missing codes:
+%
+%   CRSP code | SAS code | Description
+%      -44         .E      No valid comparison for an excess return
+%      -55         .D      No listing information
+%      -66         .C      No valid previous price or > 10 periods before
+%      -77         .B      Not trading on the current exchange
+%      -88         .A      No data available to calculate returns
+%      -99         .       No valid price (usually suspension or trading on unknown exchange)
 
-%% Subs missing return codes and 'B','C' with NaN but add additional uint8 column to map it
-%{
--66.0 Valid current price but no valid previous price. Either first price, unknown
-exchange between current and previous price, or more than 10 periods
-between time t and the time of the preceding price t’.
--77.0 Not trading on the current exchange at time t.
--88.0 No data available to calculate returns.
--99.0 Missing return due to missing price at time t; usually due to suspension
-in trading or trading on unknown exchange.
-%}
+% See for details: https://wrds-web.wharton.upenn.edu/wrds//query_forms/variable_descriptions.cfm?legacyPath=/ds/crsp/stock_a&legacyFile=msf.cfm&title=CRSP%20Annual%20Stock&libraryCode=crspa&fileCode=msf&adminEditPath=/wrds/ds/crsp/stock_a/msf_doc.cfm
 
 zipfile = 'CRSPdfequery.csv.zip';
 writeto = '.\results';
@@ -24,6 +25,7 @@ csvfile  = unzip(fullfile(path2zip, zipfile),path2zip);
 fid     = fopen(char(csvfile));
 headers = textscan(fid, '%s',1,'Delimiter','');
 headers = regexp(headers{1}{1},',','split');
+headers = [upperfirst(headers), 'MissingId'];
 
 % Block process (ret needs additional processing)
 dfequery = [];
@@ -35,20 +37,26 @@ while ~feof(fid)
     disp(c)
     txt = textscan(fid, '%u32%u32%f32%f32%f32%s%f32',N,'Delimiter',',');
     
-    % Find empty or unwanted chars in return field
-    idx = ~cellfun('isempty',regexp(txt{1,6}, '[^0-9\.-]','once')) | cellfun('isempty',txt{1,6});
-%     unique(txt{1,6}(idx,:))
+    % Find empty
+    iempty    = cellfun('isempty',txt{6});
+    
+    % Find/map missing code
+    imisscode = ~cellfun('isempty',regexp(txt{6}, '[ABCDE]','once'));
+    code      = uint8(imisscode);
+    [~,code(imisscode)]  = ismember(txt{6}(imisscode), {'A';'B';'C';'D';'E'});    
     
     % Fill them with 0s, parse into number and replace with NaNs
-    txt{1,6}(idx) = {'0'};
-    txt(1,6)      = textscan(char(txt{1,6})', '%9f64');
+    idx         = iempty | imisscode;
+    txt{6}(idx) = {'0'};
+    txt(6)      = textscan(char(txt{6})', '%9f64');
+    txt{6}(idx) = NaN;
     
     % Expand pre-allocation
     if mod(c,100) == 1
         dfequery = [dfequery; cell(100,1)];
     end
     % Convert into table
-    dfequery{c} = table(txt{:},'VariableNames',headers);
+    dfequery{c} = table(txt{:}, code, 'VariableNames',headers);
 end
 fclose(fid);
 
