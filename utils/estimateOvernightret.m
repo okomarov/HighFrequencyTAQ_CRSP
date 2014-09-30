f92704a935835a0220078b5810e529495ee82168
Oleg Komarov <o.komarov11@imperial.ac.uk>
2014-09-30 19:17:30 +0100

    [Change] overnight returns #01
    Signed-off-by: Oleg Komarov <o.komarov11@imperial.ac.uk>

--- utils/estimateOvernightret.m
+++ utils/estimateOvernightret.m
@@ -1,11 +1,13 @@
-function estimateOvernightret(tb)
+function estimateOvernightret
 
 % Load UnID - Date pairs
 uniqueID = loadresults('uniqueID');
+uniqueID.Permno = unid2permno(uniqueID.UnID);
 
 % Load Dfequery
 dfequery = loadresults('dfequery');
+dfequery = dfequery(~isnan(dfequery.Ret),{'Permno','Date','Ret'});
 
-
+[idx,pos] = ismemberb(uniqueID(:,{'Date','Permno'}), dfequery(:,{'Date','Permno'}),[2,3]);
 
 end
