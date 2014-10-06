2e9b19bc8befa9ee86369320838b54f6b30371e8
Oleg Komarov <o.komarov11@imperial.ac.uk>
2014-10-06 19:09:08 +0100

    [Change] getBetas: calls estimateBetas, more flexible syntax
    Signed-off-by: Oleg Komarov <o.komarov11@imperial.ac.uk>

--- utils/getBetas.m
+++ utils/getBetas.m
@@ -1,25 +1,31 @@
-function [betas, unids] = getBetas(issp, iscs, keeplong, matname)
+function [betas, unids] = getBetas(lookback, freq, useon, useproxy, issp, iscs, keeplong)
 % GETBETAS Loads betas, applies sp500 and/or common shares filers, unstacks
+%
+%   getBetas(issp, iscs, keeplong, matname)   
 %
 %   NOTE: Does NOT fill in between NaNs
 
-if nargin < 1 || isempty(issp),     issp     = false;   end
-if nargin < 2 || isempty(iscs),     iscs     = false;   end
-if nargin < 3 || isempty(keeplong), keeplong = true;    end
-if nargin < 4 || isempty(matname),  matname  = 'Betas'; end
+if nargin < 1 || isempty(lookback), lookback = 1;       end
+if nargin < 2 || isempty(freq),     freq     = 5;       end
+if nargin < 3 || isempty(useon),    useon    = true;    end
+if nargin < 4 || isempty(useproxy), useproxy = false;   end
+if nargin < 5 || isempty(issp),     issp     = false;   end
+if nargin < 6 || isempty(iscs),     iscs     = false;   end
+if nargin < 7 || isempty(keeplong), keeplong = false;   end
 
 % Load all betas
-betas = loadresults(matname);
-if isa(betas,'dataset'), betas = dataset2table(betas); end
+betas = estimateBetas(lookback, freq, useon, useproxy);
 
 % Filter for sp500 members
 if issp
+    fprintf('%s: filtering for sp500 members.\n', mfilename)
     idx   = issp500member(betas(:,{'Date','UnID'}));
     betas = betas(idx,:);
 end
 
 % Filter for common share (share type code 10 and 11)
 if iscs
+    fprintf('%s: filtering for common shares only.\n', mfilename)
     idx   = iscommonshare(betas(:,{'UnID','Date'}));
     betas = betas(idx,:);
 end
@@ -27,8 +33,10 @@ end
 % Remove overlapping
 [un,~,subs] = unique(betas(:,{'UnID','Date'}));
 overlap     = un(accumarray(subs,1) > 1,:);
-[~,idx]     = setdiff(betas(:,{'UnID','Date'}), overlap);
-betas       = betas(idx,:);
+if ~isempty(overlap)
+    [~,idx] = setdiff(betas(:,{'UnID','Date'}), overlap);
+    betas   = betas(idx,:);
+end
 % taq2crsp(ismember(taq2crsp.permno, taq2crsp.permno(taq2crsp.ID == overlap.ID(n))) | taq2crsp.ID == overlap.ID(n),:)
 
 if nargout == 2
