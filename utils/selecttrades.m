402a55aae0b035d2a8b8c5f1bf269970b3397053
Oleg Komarov <o.komarov11@imperial.ac.uk>
2014-10-21 19:47:07 +0100

    [Fix] selecttrades: Volume instead of size
    Signed-off-by: Oleg Komarov <o.komarov11@imperial.ac.uk>

--- utils/selecttrades.m
+++ utils/selecttrades.m
@@ -24,5 +24,5 @@ function idx = selecttrades(data)
 % - anomalous sale conditions, i.e. all but 'E' (69) Automatic Execution, 'F' (70) Intermarket Sweep Order, ' ' (32) No Sale Condition required or '@' (64) Regular Trade
 % - 0 prices
 % - size > 0
-idx = (data.G127_Correction(:,1) ~= 0 & data.G127_Correction(:,1) ~= 40) | data.G127_Correction(:,2) ~= 0 | ~all(data.Condition == ' ' | data.Condition == 'E' | data.Condition == 'F' | data.Condition == '@', 2) | data.Price == 0 | data.Size <= 0;
+idx = (data.G127_Correction(:,1) ~= 0 & data.G127_Correction(:,1) ~= 40) | data.G127_Correction(:,2) ~= 0 | ~all(data.Condition == ' ' | data.Condition == 'E' | data.Condition == 'F' | data.Condition == '@', 2) | data.Price == 0 | data.Volume <= 0;
 end
