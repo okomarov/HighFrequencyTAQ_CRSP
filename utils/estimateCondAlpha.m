a77073afbb1d04a48838e10ddb59e0e9cfebdf3a
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-03-09 16:08:44 +0000

    estimateCondAlpha take out beta estimation

--- utils/estimateCondAlpha.m
+++ utils/estimateCondAlpha.m
@@ -1,32 +1,29 @@
-function alpha = estimateCondAlpha(lookback, freq, useovern,  useproxy, sp500only, commononly)
-% estimateCondAlpha(lookback, freq, useovern,  useproxy, sp500only, commononly)
+function [alpha, rets] = estimateCondAlpha(betas, rets)
+% estimateCondAlpha(betas, rets)
 
 % Cond alphas
 % From y_it = alpha_it + beta_it * f_t + e_it, estimate:
 % 1) ^beta_it with time-series regression
 % 2) ^f_t with cross-section regression
-% 3) ^alpha_it = y_it - ^beta_it*^f_t
+% 3) ^alpha_it = y_it - ^beta_it * ^f_t
 
 % POINT 1)
-% Get/estimate betas
-beta = getBetas(lookback, freq, useovern, ~useproxy, sp500only, commononly, true);
+% Get/estimate betas (input)
 % Sort according to date
-beta = sortrows(beta,{'Date','UnID'});
+betas = sortrows(betas,{'Date','UnID'});
 
 % POINT 2)
-% Daily rets [TODO use the dsfquery from crsp]
-rets = loadresults('dailyret');
-
+% Daily rets from return_overnight (input)
 % Intersect sets, order guaranteed
-[~,ia,ib] = intersect(rets(:,{'Date','UnID'}), beta(:,{'Date','UnID'}));
+[~,ia,ib] = intersect(rets(:,{'Date','UnID'}), betas(:,{'Date','UnID'}));
 rets      = rets(ia,:);
-beta      = beta(ib,:); 
+betas     = betas(ib,:); 
 
 % Subs by day
 [~,~,subs] = unique(rets.Date);
 Fhat       = estimateCovOnVar(betas.Beta, rets.Totret, subs);
 
 % POINT 3)
-beta.Alpha = rets.Dret - Fhat(subs).*beta.Beta;
-alpha = beta;
+betas.Alpha = rets.Totret - Fhat(subs).*betas.Beta;
+alpha = betas;
 end
