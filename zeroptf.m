489d68f39f49f7aa239cc88792a669523cec19f3
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-03-11 22:27:57 +0000

    Added zeroptf

---
+++ zeroptf.m
@@ -0,0 +1,81 @@
+function [stratret, tbstats, tbarets] = zeroptf(tb)
+% [stratret, tbstats, tbarets] = zeroptf(tb, lookback)
+
+% Remove rows with no score
+tb = tb(~isnan(tb.Score),:);
+
+% Unstack scores
+score = unstack(tb(:,{'UnID','Date','Score'}),'Score','UnID');
+score = sortrows(score,'Date');
+dates = uint32(score.Date);
+score = table2array(score(:,2:end));
+
+% Unstack returns
+ret = unstack(tb(:,{'UnID','Date','Ret'}),'Ret','UnID');
+ret = sortrows(ret,'Date');
+ret = table2array(ret(:,2:end));
+
+% Beginning of month rebalancing scheme
+[~, ~, subs] = unique(dates./100);
+rebdate      = find([false; diff(subs)>0]);
+
+N        = numel(dates);
+stratret = NaN(N, 1);
+for ii = 1:numel(rebdate)
+    [iShort, iLong] = deal(false(1,size(ret,2)));
+    % Alive in the future
+    ifut    = subs == ii+1;
+    isalive = any(score(ifut,:));
+    
+    % Score ranking
+    scores          = score(rebdate(ii)-1,isalive);
+    ptiles          = prctile(scores,[10,90]);
+    iShort(isalive) = scores <= ptiles(1);
+    iLong (isalive) = scores >= ptiles(2);
+       
+    % Equal weighted strategy
+    stratret(ifut) = nanmean([ret(ifut,iLong), -ret(ifut,iShort)],2);
+end
+
+plotdates = yyyymmdd2datetime(dates);
+from      = find(~isnan(stratret),1,'first');
+stratlvl  = [NaN(from-2,1); cumprod([1; stratret(from:end)+1])];
+
+if nargout == 0
+    plot(plotdates, stratlvl)
+end
+
+[tbstats, tbarets] = stratstats(stratlvl(from-1:end,:),plotdates(from-1:end));
+end
+
+function [tbstats, tbarets] = stratstats(lvl,dates)
+monthrets     = level2mrets(lvl,dates);
+tbstats.Annret     = lvl(end,:)'.^(1/years(dates(end)-dates(1)))-1;
+tbstats.Annstd     = std(monthrets)'*sqrt(12);
+tbstats.Downstd    = std(monthrets > 0 .* monthrets)' * sqrt(12);
+tbstats.Minret     = min(monthrets)';
+tbstats.Maxret     = max(monthrets)';
+tbstats.IR         = tbstats.Annret./tbstats.Annstd;
+[tbstats.Mdd,imdd] = maxdrawdown(lvl);
+tbstats.Mdd        = tbstats.Mdd';
+tbstats.Reclen     = days(dates(imdd(end,:))-dates(imdd(1,:)));
+tbstats.Sortino    = tbstats.Annret./tbstats.Downstd;
+
+tbstats = struct2table(tbstats);
+tbarets = table(unique(year(dates)), level2arets(lvl,dates),'VariableNames',{'Year','Ret'});
+end
+
+
+function mrets = level2mrets(lvl,dates)
+sz    = size(lvl);
+rsub  = repmat(cumsum([1; logical(diff(month(dates)))]), 1, sz(2));
+csub  = repmat(1:sz(2),sz(1),1);
+mrets = accumarray([rsub(:),csub(:)],lvl(:), [],@(x) x(end)./x(1)-1); 
+end
+
+function arets = level2arets(lvl,dates)
+sz    = size(lvl);
+rsub  = repmat(cumsum([1; logical(diff(year(dates)))]), 1, sz(2));
+csub  = repmat(1:sz(2),sz(1),1);
+arets = accumarray([rsub(:),csub(:)],lvl(:), [],@(x) x(end)./x(1)-1); 
+end
