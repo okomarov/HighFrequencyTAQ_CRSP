7101b7bffce61ec52e0039dfd409fd8823cab224
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-04-02 22:23:42 +0100

    overnight redone

--- utils/estimateOvernightretIntraday.m
+++ utils/estimateOvernightretIntraday.m
@@ -9,17 +9,18 @@ try
 catch
     % Median price
     cached = loadresults('medianprice');
-idx      = dsfquery.Shrcd == 10 | dsfquery.Shrcd == 11;
-dsfquery = dsfquery(idx,:);
-dsfquery = dsfquery(~isnan(dsfquery.Ret),{'Permno','Date','Ret'});
-
-% % Winsorize returns at 0.1 and 99.9%
-% ptiles = prctile(dsfquery.Ret,[0.1,99.9]);
-% % boxplot(dsfquery.Ret)
-% % idx    = in(dsfquery.Ret, ptiles);
-% % boxplot(dsfquery.Ret(idx))
-% dsfquery.Ret(dsfquery.Ret < ptiles(1)) = ptiles(1);
-% dsfquery.Ret(dsfquery.Ret > ptiles(2)) = ptiles(2);
+    
+    % Add File
+    path2data   = '.\data\TAQ\';
+    master      = load(fullfile(path2data,'master'),'-mat');
+    keyA        = uint64(cached.Id) * 1e8 + uint64(cached.Date);
+    keyB        = uint64(master.mst.Id) * 1e8 + uint64(master.mst.Date);
+    [~,pos]     = ismember(keyA, keyB);
+    cached.File = master.mst.File(pos);
+    clear master
+    
+    res = Analyze('return_intraday',[],cached,path2data);
+end
 
 % Load UnID - Date pairs and keep common shares only
 uniqueID = loadresults('uniqueID');
@@ -28,32 +29,38 @@ uniqueID = uniqueID(iscommonshare(uniqueID(:,{'UnID','Date'})),:);
 % Map permno
 uniqueID.Permno = unid2permno(uniqueID.UnID);
 
-% Map totret to uniqueID (by combined key to speed up)
-keyA           = uint64(uniqueID.Permno) * 1e8 + uint64(uniqueID.Date);
-keyB           = uint64(dsfquery.Permno) * 1e8 + uint64(dsfquery.Date);
-[idx,pos]      = ismember(keyA, keyB);
-uniqueID       = uniqueID(idx,:);
-uniqueID.RetCC = NaN(size(uniqueID.Permno));
-uniqueID.RetCC = dsfquery.Ret(pos(idx));
+% Add RetOC
+keyA           = uint64(uniqueID.Id) * 1e8 + uint64(uniqueID.Date);
+keyB           = uint64(res.Id)      * 1e8 + uint64(res.Date);
+[~,pos]        = ismember(keyA, keyB);
+uniqueID.RetOC = res.RetOC(pos);
+
+% Load dsfquery (filtering for common shares done indirectly)
+dsfquery = loadresults('dsfquery');
+
+% % Winsorize returns at 0.1 and 99.9%
+ptiles = prctile(dsfquery.Ret,[0.1,99.9]);
+% boxplot(dsfquery.Ret)
+% idx    = in(dsfquery.Ret, ptiles);
+% boxplot(dsfquery.Ret(idx))
+dsfquery.Ret(dsfquery.Ret < ptiles(1)) = ptiles(1);
+dsfquery.Ret(dsfquery.Ret > ptiles(2)) = ptiles(2);
+
+% Add RetCC from dsfqwuery
+keyA                = uint64(uniqueID.Permno) * 1e8 + uint64(uniqueID.Date);
+keyB                = uint64(dsfquery.Permno) * 1e8 + uint64(dsfquery.Date);
+[idx,pos]           = ismember(keyA, keyB);
+uniqueID.RetCC      = NaN(size(uniqueID.Permno));
+uniqueID.RetCC(idx) = dsfquery.Ret(pos(idx));
 clear dsfquery
 
-% Add median price
-res               = loadresults('medianprice');
-keyA              = uint64(uniqueID.Id) * 1e8 + uint64(uniqueID.Date);
-keyB              = uint64(res.Id)      * 1e8 + uint64(res.Date);
-[~,pos]           = ismember(keyA, keyB);
-uniqueID.MedPrice = res.MedPrice(pos);
+% Overnight return
+uniqueID.RetCO = log((1 + uniqueID.RetCC)./(1 + uniqueID.RetOC )); 
 
-% Add File, From and To
-path2data     = '.\data\TAQ\';
-master        = load(fullfile(path2data,'master'),'-mat');
-keyB          = uint64(master.mst.Id) * 1e8 + uint64(master.mst.Date);
-[~,pos]       = ismember(keyA, keyB);
-uniqueID.File = master.mst.File(pos);
-uniqueID.From = master.mst.From(pos);
-uniqueID.To   = master.mst.To(pos);
-clear master
-uniqueID(:,{'Permno'}) = [];
+% Filter out NaNs
+idx = ~(isnan(uniqueID.RetCC) | isnan(uniqueID.RetOC));
+res = uniqueID(idx,:);
 
-res = Analyze('return_overnight_intraday',[],uniqueID,path2data);
+filename = sprintf('%s_%s.mat',datestr(now,'yyyymmdd_HHMM'),'return_intraday_overnight');
+save(fullfile('.\results\',filename), 'res')
 end
