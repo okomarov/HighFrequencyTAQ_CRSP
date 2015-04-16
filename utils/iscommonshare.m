e2c4a14f627bf93cbf454d9e18557c727403efa0
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-04-16 19:55:25 +0100

    rewrite mapShrcd to go through Permno

--- utils/iscommonshare.m
+++ utils/iscommonshare.m
@@ -1,13 +1,13 @@
 function tf = iscommonshare(tb)
-% ISCOMMONSHARE Checks which UnID - Date pairs are common shares (share type code 10 and 11) 
+% ISCOMMONSHARE Checks which Id - Date pairs are common shares (share type code 10 and 11) 
 %
-%   ISCOMMONSHARE(TB) TB is a table with UnID and yyyymmdd Date 
+%   ISCOMMONSHARE(TB) TB is a table with Id and yyyymmdd Date 
 if isa(tb,'dataset')
     tb = dataset2table(tb);
 end
 
 shrcd = loadresults('shrcd');
-shrcd = shrcd(shrcd.Shrcd == 11 | shrcd.Shrcd == 10,{'UnID','Date'});
-tf    = ismembIdDate(tb.UnID, tb.Date, shrcd.UnID, shrcd.Date);
+shrcd = shrcd(shrcd.Shrcd == 11 | shrcd.Shrcd == 10,{'Id','Date'});
+tf    = ismembIdDate(tb.Id, tb.Date, shrcd.Id, shrcd.Date);
 
 end
--- utils/mapShrcd2mst.m
+++ utils/mapShrcd2mst.m
@@ -2,87 +2,57 @@ function shrcd = mapShrcd2mst
 
 % MAPSHRCD2MST Maps share code type to master records with Id and Date and Symbol
 
+% Load master permno
+mst = loadresults('masterPermno');
+
 % Load msenames
 try
     msenames = loadresults('msenames');
 catch
     msenames = importMsenames('.\data\CRSP\');
 end
-% Sort
-msenames = sortrows(msenames(:,{'PERMNO','NAMEDT','SHRCD'}));
+msenames             = msenames(msenames.NAMEENDT > 19923112,:);
+idx                  = msenames.NAMEDT <= 19923112;
+msenames.NAMEDT(idx) = 19930101;
+
+% Filter by permno
+idx      = ismember(msenames.PERMNO, unique(mst.Permno));
+msenames = msenames(idx,:);
+
 % Time consolidation
+msenames = sortrows(msenames(:,{'PERMNO','NAMEDT','SHRCD'}));
 idx      = isfeatchange(msenames(:,{'PERMNO','SHRCD','NAMEDT'}));
 msenames = msenames(idx,:);
 
-% Load taq2crsp
-taq2crsp = loadresults('taq2crsp');
-if isa(taq2crsp,'dataset'), taq2crsp = dataset2table(taq2crsp); end
-taq2crsp = taq2crsp(~isnan(taq2crsp.permno),{'ID','permno','datef'});
-% Time consolidation
-taq2crsp = sortrows(taq2crsp,{'ID','datef'});
-idx      = isfeatchange(taq2crsp);
-taq2crsp = taq2crsp(idx,:);
-
-% Unstack msenames into shrcd
-msenames       = msenames(ismember(msenames.PERMNO, taq2crsp.permno),:);
-msenames.SHRCD = single(msenames.SHRCD); % For NaN padding in unstack
-shrcd          = unstack(msenames,'SHRCD','PERMNO');
-shrcd          = sortrows(shrcd,'NAMEDT');
-% Fill gaps
-names          = getVariableNames(shrcd);
-shrcd(:,2:end) = varfun(@nanfillts,shrcd(:,2:end));
-shrcd          = setVariableNames(shrcd,['Date', names(2:end)]);
-
-% Unstack taq2crsp into mask
-id2permno = unique(taq2crsp(:,{'ID','permno'}));
-id2permno.ID = uint16(id2permno.ID);
-import matlab.lang.*
-id2permno.Properties.RowNames = makeValidName(cellstr(num2str(id2permno.ID)));
-taq2crsp.Val = ones(size(taq2crsp,1),1,'single');
-mask     = unstack(taq2crsp(:,{'Val','ID','datef'}),'Val','ID');
-mask     = sortrows(mask,'datef');
-mask.Properties.VariableNames{1} = 'Date';
+% Unstack
+msenames = convertColumn(msenames,'double','SHRCD');
+msenames = unstack(msenames,'SHRCD','PERMNO');
 
 % Sample dates
-uniqueID = loadresults('uniqueID');
-refdates = unique(uniqueID.Date);
-shrcd    = sampledates(shrcd,refdates);
-mask     = sampledates(mask,refdates);
+msenames = renameVarNames(msenames, 'Date','NAMEDT');
+msenames = sampledates(msenames, unique(mst.Date));
 
-% Create mask 
-mask = table2array(mask(:,2:end));
-mask = logical(uint8(nanfillts(mask)));
+% Stack back into tall table
+vnames   = getVariableNames(msenames);
+msenames = stack(msenames, vnames(2:end), ...
+                'IndexVariableName', 'Permno',...
+                'NewDataVariableName','Shrcd');
 
-% Sample shrcd from msenames
-permnos2get = makeValidName(cellstr(num2str(id2permno.permno)));
-shrcd = uint8(table2array(shrcd(:, permnos2get)));
+% Keep only non 0 values
+msenames = msenames(msenames.Shrcd ~= 0,:);
 
-% Apply mask 
-shrcd(~mask) = 0; clear mask
+% Convert literal xPermno to numeric id
+Permno          = char(msenames.Permno);
+Permno          = Permno(:,2:end)';
+Permno          = textscan([Permno; repmat(' ',1,size(Permno,2))],'%u32');
+msenames.Permno = Permno{1};
 
-shrcd = array2table(shrcd,'VariableNames',id2permno.Properties.RowNames);
-shrcd.Date = refdates;
-
-% Stack back
-shrcd = stack(shrcd,id2permno.Properties.RowNames,'NewDataVariableName','Shrcd',...
-              'IndexVariableName','UnID');
-
-% Clean up
-shrcd = shrcd(shrcd.Shrcd ~= 0,:);
-[~,pos] = ismember(shrcd.UnID,id2permno.Properties.RowNames);
-shrcd.UnID = id2permno.ID(pos); clear pos
-
-% Map to uniqueID (in blocks)
-uniqueID.Shrcd =  zeros(size(uniqueID,1),1,'uint8');
-N = 4;
-edges = fix(linspace(0,size(shrcd,1),N+1));
-for ii = 1:N
-    [idx,pos] = ismember(uniqueID(:,{'Date','UnID'}),shrcd(edges(ii)+1:edges(ii+1),{'Date','UnID'}));
-    uniqueID.Shrcd(idx) = shrcd.Shrcd(pos(idx)+edges(ii));
-end
+% Map to mst
+[idx,pos]        = ismembIdDate(mst.Permno,mst.Date, msenames.Permno, msenames.Date);
+mst.Shrcd(idx,1) = uint8(msenames.Shrcd(pos(idx)));
 
 % Save
-shrcd = uniqueID;
+shrcd = mst;
 save(fullfile('.\results',sprintf('%s_%s.mat',datestr(now,'yyyymmdd_HHMM'),'shrcd')), 'shrcd')
 
 end
