469b6efa1ed00fdd43c1733da230ff3ecd13cbad
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-04-16 19:34:29 +0100

    SP500 membership through Permno and Id

--- utils/issp500member.m
+++ utils/issp500member.m
@@ -1,104 +1,55 @@
 function tf = issp500member(tb)
-% ISSP500MEMBER Checks which UnID - Date pair are sp500 members
+% ISSP500MEMBER Checks which Id - Date pair are sp500 members
 %
-%   ISSP500MEMBER(TB) TB is a table with UnID and yyyymmdd Date 
+%   ISSP500MEMBER(TB) TB is a table with Id and yyyymmdd Date 
 
 
 if isa(tb,'dataset')
     tb = dataset2table(tb);
 end
+
 % Load sp500 membership table
 spconst = loadresults('spconst');
 
-try
-    taq2crsp = loadresults('taq2crsp_sp500');
-catch
-    % Load unid2permno
-    taq2crsp = loadresults('taq2crsp');
-   
-    % Keep only match score 10
-    taq2crsp = taq2crsp(taq2crsp.score == 10,:);
-        
-    % Filter out non members
-    taq2crsp   = dataset2table(taq2crsp(ismember(taq2crsp.permno, spconst.Id),:));
-    
-    % Drop tickers with suffixes
-    [~,~,subs] = unique(taq2crsp.permno);
-    N          = numel(subs);
-    idrop      = false(N,1);
-    
-    % LOOP for each permno
-    for ii = 1:N
-        % Index permno and corresponding tickers
-        isubs   = subs == ii;
-        symbols = taq2crsp.symbol(isubs);
-        nsymb   = nnz(isubs);
-        
-        if nsymb > 1
-            idx = false(nsymb, 1);
-            for jj = 1:nsymb
-                if all(idx), break,end
-                % Mark to drop if substring
-                idx = idx | ~cellfun('isempty', regexp(symbols, sprintf('^%s\\w+',symbols{jj}),'once'));
-            end
-            if any(idx), idrop(isubs) = idx; end
-        end
-    end
-    taq2crsp = taq2crsp(~idrop,:);
+% Sample at refdates
+refdates = unique(tb.Date);
+spconst  = sampledates(spconst.Panel, refdates,false);
 
+% Stack back into tall table
+vnames  = getVariableNames(spconst);
+spconst = stack(spconst, vnames(2:end), ...
+                'IndexVariableName', 'Permno',...
+                'NewDataVariableName','Issp500');
+
+% Keep only SP500 members
+spconst = spconst(spconst.Issp500 ~= 0,:);
+
+% Convert literal xPermno to numeric id
+Permno         = char(spconst.Permno);
+Permno         = Permno(:,2:end)';
+Permno         = textscan([Permno; repmat(' ',1,size(Permno,2))],'%u32');
+spconst.Permno = Permno{1};
+
+% Load Permno to Id mapping
+try
+    permno2id = loadresults('masterPermnoSP500');
+catch
+    % Load Permno - date pairs
+    permno2id = loadresults('masterPermno');
+   
+    % Filter out non members
+    idx       = ismember(permno2id.Permno, spconst.Id);
+    permno2id = permno2id(idx,:);
+    
     % Cache results
-    fname = fullfile('..\results\',sprintf('%s_%s.mat',datestr(now,'yyyymmdd_HHMM'),'taq2crsp_sp500'));
-    save(fname, 'taq2crsp')
+    fname = fullfile('.\results\',sprintf('%s_%s.mat',datestr(now,'yyyymmdd_HHMM'),'masterPermnoSP500'));
+    save(fname,'id2permno')
 end
 
-% Map UnID to permno
-[idx,pos]      = ismember(tb.UnID, taq2crsp.ID);
-tbsmall        = tb(idx,'Date');
-tbsmall.Permno = taq2crsp.permno(pos(idx));
+% Map membership to Id
+idx       = ismembIdDate(permno2id.Permno, permno2id.Date,spconst.Permno, spconst.Date);
+permno2id = permno2id(idx,:);
 
-% Remove overlapping
-[un,~,subs] = unique(tbsmall);
-overlap     = un(accumarray(subs,1) > 1,:);
-ioverlap    = ismembIdDate(tbsmall.Permno, tbsmall.Date, overlap.Permno, overlap.Date);
-tbsmall     = tbsmall(~ioverlap,:);
-idx(idx)    = idx(idx) & ~ioverlap;
-
-% taq2crsp(ismember(taq2crsp.permno, unique(overlap.Permno)),:)
-
-% Pivot tb
-warning off MATLAB:table:ModifiedVarnames
-nrows       = size(tbsmall,1);
-tbsmall.Val = ones(nrows,1,'uint8');
-tbpanel = unstack(tbsmall,'Val','Permno');
-permnos = unique(tbsmall.Permno);
-warning on MATLAB:table:ModifiedVarnames
-
-% Intersect permnos
-tbnames = getVariableNames(tbpanel); 
-spnames = getVariableNames(spconst.Panel); 
-[~,itb,isp] = intersect(tbnames, spnames);
-tbpanel = table2array(tbpanel(:,itb));
-sppanel = table2array(spconst.Panel(:,isp));
-
-% Intersect/expand dates
-refdates       = tbpanel(:,1);
-alldates       = union(sppanel(:,1), refdates);
-[~,pdates]     = ismember(sppanel(:,1),alldates);
-data           = NaN(numel(alldates),size(sppanel,2)-1);
-data(pdates,:) = sppanel(:,2:end);
-% Fill in previous value and get reference dates only
-data           = nanfillts(data,1);
-sppanel        = [refdates data(ismember(alldates, refdates),:)];
-
-tbpanel(~sppanel) = 0;
-tbpanel = array2table(tbpanel,'VariableNames',tbnames);
-tbpanel = stack(tbpanel,tbnames(2:end),'NewDataVariableName','Val','IndexVariableName','Permno');
-tbpanel = tbpanel(tbpanel.Val == 1,1:2);
-[~,pos] = ismember(tbpanel.Permno,tbnames(2:end));
-tbpanel.Permno = permnos(pos);
-
-% Speed up with composite key
-ismall  = ismembIdDate(tbsmall.Permno, tbsmall.Date, tbpanel.Permno, tbpanel.Date);
-tf      = false(size(tb,1),1);
-tf(idx) = ismall;
+% Filter out tb
+tf = ismembIdDate(tb.Id, tb.Date, permno2id.Id, permno2id.Date);
 end
