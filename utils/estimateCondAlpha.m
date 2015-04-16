384c19b93b6c83eeb635a55e699b4af7673dcdbe
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-04-16 16:18:13 +0100

    Corrected conditional alphas

--- utils/estimateCondAlpha.m
+++ utils/estimateCondAlpha.m
@@ -1,4 +1,4 @@
-function [betas, rets] = estimateCondAlpha(betas, rets, f)
+function scores = estimateCondAlpha(lookback, betas, rets, f, C)
 % estimateCondAlpha(betas, rets)
 % Sorts betas by Date and UnID
 
@@ -8,38 +8,56 @@ function [betas, rets] = estimateCondAlpha(betas, rets, f)
 % 2) estimate the conditional alpha E(^Z_{i,t+1}|C_t)
 
 % Unstack returns and betas
-rets = unstack(rets(:,{'Date','UnID','RetCC'}),'RetCC','UnID');
+rets = rets(~isnan(rets.RetCC),{'Date','UnID','RetCC'});
+rets = unstack(rets,'RetCC','UnID');
 rets = sortrows(rets,'Date');
 
+betas = betas(~isnan(betas.Beta),{'Date','UnID','Beta'});
 betas = unstack(betas(:,{'Date','UnID','Beta'}),'Beta','UnID');
 betas = sortrows(betas,'Date');
 
 % Intersect returns and betas
 [~,ia,ib] = intersect(getVariableNames(betas),getVariableNames(rets));
-betas = betas(:,ia);
-rets = rets(:,ib);
+betas     = betas(:,ia);
+rets      = rets(:,ib);
 
 % Ensure f sorted by date
-f     = sortrows(f    ,'Date');
+f = sortrows(f(:,{'Date','RetCC'}),'Date');
 
-% Point betas to next day returns
-[idx,pos] = ismember(betas.Date, f.Date(1:end-1));
-from      = find(idx, 1,'first')-1;
-betas     = betas(from:end,:);
-pos       = pos(from:end);
-f         = f.RetCC(pos+1);
+% Reference dates
+refdates = intersect(rets.Date, f.Date);
 
-[~  ,pos] = ismember(betas.Date, rets.Date(1:end-1,:));
-rets      = rets(pos+1,:);
+% Time align
+notrail = true;
+% Sample at t-1
+betas   = sampledates(betas,refdates-1,notrail);
+C       = sampledates(C    ,refdates-1,notrail);
+% Sample at t
+rets    = sampledates(rets ,refdates  ,notrail);
+f       = sampledates(f    ,refdates  ,notrail);
 
 % Residual returns
-Z = rets;
-for ii = 2:width(rets)
-    Z{:,ii} = rets{:,ii} - betas{:,ii}.*f;
+nobs = size(rets,1);
+Z    = NaN(nobs, width(rets));
+for c = 2:width(rets)
+    Z(:,c) = rets{:,c} - betas{:,c}.*f.RetCC;
 end
 
-
 % POINT 2)
+scores = NaN(nobs, width(rets));
+X      = C{:,2:end};
+for r = lookback:nobs
+    pos = r-lookback+1:r;
+    x   = X(pos,:);
+    for c = 2:width(rets)
+        y = Z(pos,c);
+        if nnz(~isnan(y)) > 10
+            mdl = regstats(y,x,'linear','tstat');
+            scores(r,c) = mdl.tstat.beta(1);
+        end
+    end
+end
 
-betas.Alpha = rets.Totret - Fhat(subs).*betas.Beta;
+scores = array2table(scores,'VariableNames',getVariableNames(rets));
+scores.Date = rets.Date;
 end
