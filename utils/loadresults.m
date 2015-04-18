6ec93fa682f1c83073087e2e1916319a0ddc939d
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-04-18 21:42:26 +0100

    moe fexibility to loadresults

--- utils/loadresults.m
+++ utils/loadresults.m
@@ -4,7 +4,10 @@ function out = loadresults(name)
 %   LOADRESULTS(NAME) Loads the latest '*NAME.mat'
 
 resdir  = '.\results';
-files   = dir(fullfile(resdir, sprintf('*%s.mat', name)));
+if isempty(strfind(name,'.mat'))
+    name = [name, '.mat'];
+end
+files   = dir(fullfile(resdir, ['*', name]));
 if isempty(files)
     error('loadresults:nofile','No files matching ''%s'' found.',name)
 end
