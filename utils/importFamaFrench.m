954e520659bc36f8029acf851a23040873424eb6
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-05-10 20:00:09 +0100

    Enhanced to import all Fama and French data

--- utils/importFamaFrench.m
+++ utils/importFamaFrench.m
@@ -1,30 +1,98 @@
-function FFfactors = importFamaFrench(path2zip)
-% IMPORTFAMAFRENCH Imports the zipped Fama and French factors
+function data = importFamaFrench(zipname, outdir)
+% IMPORTFAMAFRENCH Imports datasets from Kenneth French's Data Library webpage
 %
-%   IMOPRTMSENAMES(PATH2ZIP)
-% 
+%   IMPORTFAMAFRENCH(ZIPNAME) Imports into a table the dataset specified 
+%                             by ZIPNAME.
+%
+%   IMPORTFAMAFRENCH(...,OUTDIR) Specify in which folder to save the
+%                                imported data. 
+%                                By default it will be  saved under 
+%                                '.\results\ZIPNAME.mat'
+%
+%   IMPORTFAMAFRENCH() Lists available datasets, their ZIPNAMEs and the
+%                      description.
+%                       
+%
 % See <a href="http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html">Fama French data</a>
 
-zipfile = 'F-F_Research_Data_Factors_daily.zip';
-writeto = '.\results';
+url = 'http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/';
 
-% Unzip and loadf as row strings
-txtfile = unzip(fullfile(path2zip, zipfile),path2zip);
-fid     = fopen(char(txtfile));
-vnames  = textscan(fid, '%s%s%s%s',1,'MultipleDelimsAsOne',true,'HeaderLines',4);
-txt     = textscan(fid, '%u%f%f%f%f','MultipleDelimsAsOne',true);
+% List available datasets on Kennet's page
+if nargin < 1
+    list = listAvailableData(url);
+    data = list;
+    return
+end
+if nargin < 2
+    outdir = '.\results';
+end
+
+% Try to load from web
+try
+    txtfile = unzip([url, 'ftp/', zipname], tempdir);
+catch
+    error('Problem with importing')
+end
+
+% Parse .txt dataset:
+%   Usually has few lines of description followed by one blank line, one
+%   line of variable names, and then data. Uses multiple whitespaces as
+%   delimiter.
+fid      = fopen(char(txtfile));
+tline    = fgetl(fid);
+isHeader = false;
+Desc     = '';
+while ischar(tline)
+    % Are we parsing the header line? 
+    if isHeader
+        vnames = textscan(tline, '%s','MultipleDelimsAsOne',true);
+        vnames = vnames{1};
+        vnames = strrep(vnames,'-','Minus');
+        break
+    end
+    % Empty line, next is header
+    if isempty(tline)
+        isHeader = true;
+    else
+        Desc = [Desc, tline];
+    end
+    tline = fgetl(fid);
+end
+% Parse data
+fmt = ['%u' repmat('%f',1,numel(vnames))];
+txt = textscan(fid, fmt,'MultipleDelimsAsOne',true);
 fclose(fid);
 
 % Convert percentage returns to decimal
 txt(2:end) = cellfun(@(x) x/100, txt(2:end),'un',0);
 
 % Convert into table
-FFfactors = table(txt{:},'VariableNames',['Date', 'MktMinusRF', vnames{2:end}]);
+data                        = table(txt{:},'VariableNames',['Date'; vnames(:)]);
+data.Properties.Description = Desc;
 
-% Delete unzipped .txt
+% Delete temporary .txt
 delete(txtfile{:})
 
 % Save
-fname = sprintf('%s_%s.mat',datestr(now,'yyyymmdd_HHMM'),'FFfactors');
-save(fullfile(writeto, fname), 'FFfactors')
+[~,name] = fileparts(zipname);
+save(fullfile(outdir,[name,'.mat']), 'data')
+end
+
+function list = listAvailableData(url)
+% Import webpage
+str          = webread([url,'/data_library.html']);
+% Parse links
+[Link,enpos] = regexp(str, '<a href ?= ?"([^"]*)">','tokens','end');
+Link         = cat(1,Link{:});
+% Get zipnames
+idx          = ~cellfun('isempty',regexp(Link,'.zip','once'));
+Zipname      = strrep(Link(idx),'ftp/','');
+enpos        = enpos(idx);
+list         = table(Zipname);
+% Extract description
+for ii = 1:numel(enpos)
+    p                      = enpos(ii);
+    tmp                    = regexprep(str(p+1:p+150),'(<[ba]>|</b>)','');
+    list.Description(ii,1) = strtrim(regexp(tmp, '.*(?=</a>)','match'));
+end
 end
