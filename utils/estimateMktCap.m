cdc8de1e21733e1896601d5a1b943828898d3152
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-05-10 22:03:22 +0100

    Generic mktCap

--- utils/estimateMktCap.m
+++ utils/estimateMktCap.m
@@ -1,36 +1,40 @@
 function res = estimateMktCap()
-% Load data
-id2permno = loadresults('masterPermno');
 
+from = 19930101;
+
+% Load prices
 dsfquery     = loadresults('dsfquery');
 dsfquery     = dsfquery(:,{'Permno', 'Date','Prc'});
 dsfquery.Prc = abs(dsfquery.Prc);
+idx          = dsfquery.Date >= from;
+dsfquery     = dsfquery(idx,:);
+to           = max(dsfquery.Date);
 
-dseshares = loadresults('dseshares');
-dseshares = unique(dseshares(:,{'PERMNO', 'SHRSDT','SHRENDDT','SHROUT'}));
+% Load shares
+dseshares               = loadresults('dseshares');
+dseshares               = unique(dseshares(:,{'PERMNO', 'SHRSDT','SHRENDDT','SHROUT'}));
+idx                     = dseshares.SHRENDDT >= from;
+dseshares               = dseshares(idx,:);
+idx                     = dseshares.SHRENDDT > to;
+dseshares.SHRENDDT(idx) = to;
+idx                     = dseshares.SHRSDT < from;
+dseshares.SHRSDT(idx)   = from;
 
-% Reference permnos and dates
-Permno = intersect(dsfquery.Permno, dseshares.PERMNO);
-Permno = intersect(Permno, id2permno.Permno);
-Date   = unique(id2permno.Date);
-sz     = [numel(Date), numel(Permno)];
-
-% Filter
-idx      = ismember(dsfquery.Permno,Permno) & ismember(dsfquery.Date,Date);
-dsfquery = dsfquery(idx,:);
-
-idx       = ismember(dseshares.PERMNO, Permno) & dseshares.SHRENDDT >= min(Date);
+% Intersect permnos
+Permno    = intersect(dsfquery.Permno, dseshares.PERMNO);
+idx       = ismember(dsfquery.Permno,Permno);
+dsfquery  = dsfquery(idx,:);
+idx       = ismember(dseshares.PERMNO, Permno);
 dseshares = dseshares(idx,:);
 
+Date = unique(dsfquery.Date);
+
 % Unstack prices
-[~,row]        = ismember(dsfquery.Date, Date);
-[~,col]        = ismember(dsfquery.Permno, Permno);
-dsfquery       = accumarray([row,col], dsfquery.Prc, sz);
-inan           = ~accumarray([row,col], 1, sz);
-dsfquery(inan) = NaN;
-dsfquery       = nanfillts(dsfquery,true);
+dsfquery = sortrows(tbextend.unstack(dsfquery,'Prc','Permno'),'Date');
+dsfquery = nanfillts(dsfquery{:,2:end},true);
 
 % Unstack number of shares
+dseshares        = consolidateFromTo(dseshares);
 dseshares        = convertColumn(dseshares,'double','SHROUT');
 dseshares        = pivotFromTo(dseshares);
 irow             = ismember(dseshares.Date, Date);
@@ -44,7 +48,7 @@ dseshares        = nanfillts(dseshares);
 % Market capitalizations
 res = dseshares.*dsfquery;
 
-% Unstack
+% Stack back
 [Date,Permno] = ndgrid(Date,Permno);
 notnan        = ~isnan(res);
 res           = table(Date(notnan), Permno(notnan), res(notnan),...
