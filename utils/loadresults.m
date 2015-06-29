854d14cd95d7a51041c4ab419f5e2cbbf5160f70
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-06-29 18:31:30 +0100

    flexibility: loads multiple vars and results fodler input

--- utils/loadresults.m
+++ utils/loadresults.m
@@ -1,18 +1,29 @@
-function out = loadresults(name)
+function out = loadresults(name, resdir)
 % LOADRESULTS Load results from .\results
 %
 %   LOADRESULTS(NAME) Loads the latest '*NAME.mat'
 
-resdir  = '.\results';
+if nargin < 2
+    resdir  = '.\results';
+end
+
 if isempty(strfind(name,'.mat'))
     name = [name, '.mat'];
 end
+% Most recent
 files   = dir(fullfile(resdir, ['*', name]));
 if isempty(files)
     error('loadresults:nofile','No files matching ''%s'' found.',name)
 end
-[~,idx] = max([files.datenum]); % Most recent
+[~,idx] = max([files.datenum]); 
 s       = load(fullfile(resdir,files(idx).name));
-out     = s.(char(fieldnames(s)));
+% Load whole structure if multiple fields
+fnames  = fieldnames(s);
+if numel(fnames) == 1
+    out = s.(fnames{1});
+else
+    out = s;
+end
+% Convert datasets to table
 if isa(out, 'dataset'), out = dataset2table(out); end
 end
