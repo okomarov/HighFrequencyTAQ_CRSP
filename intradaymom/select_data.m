b12d95d71d06616053618f26a5b5335caebc635b
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-09-23 17:37:04 +0100

    fix price_fl selection

--- intradaymom/select_data.m
+++ intradaymom/select_data.m
@@ -1,10 +1,10 @@
-OPT_EDGES_BAD_PRICES = [.5,1.5];
+OPT_EDGES_BAD_PRICES = [];
 
 %% Import data
 
 % Index data
-datapath = '..\data\TAQ\sampled\5min\new';
-master = load(fullfile(datapath,'master'),'-mat');
+datapath = '..\data\TAQ\sampled\5min\nobad';
+master   = load(fullfile(datapath,'master'),'-mat');
 
 % Sample first and last price
 try
@@ -16,12 +16,11 @@ catch
     else
         mst = mst(:, {'File','Id','Permno','Date','MedPrice', 'Isbadday','Isfewobs'});
     end
-    price_fl = Analyze('sampleFirstLast',[],mst,[],[], struct('edges',[]));
+    price_fl = Analyze('sampleFirstLast',[],mst,[],[], struct('edges',OPT_EDGES_BAD_PRICES));
 end
-[idx,pos] = ismembIdDate(price_fl.Permno, price_fl.Date, master.mst.Permno, master.mst.Date);
-tmp(pos(idx),:) = price_fl(idx,:);
-price_fl = tmp;
-price_fl.File = master.mst.File;
+[idx,pos] = ismembIdDate(master.mst.Permno, master.mst.Date, price_fl.Permno, price_fl.Date);
+price_fl  = price_fl(pos(idx),:);
+
 
 % Common shares
 shrcd      = loadresults('shrcd','../results');
