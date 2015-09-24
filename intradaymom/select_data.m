d49c05a016f4e1deaa6541982b52f24c5909e09e
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-09-24 21:13:57 +0100

    vwap in select data

--- intradaymom/select_data.m
+++ intradaymom/select_data.m
@@ -21,16 +21,37 @@ end
 [idx,pos] = ismembIdDate(master.mst.Permno, master.mst.Date, price_fl.Permno, price_fl.Date);
 price_fl  = price_fl(pos(idx),:);
 
+% Sample VWAP
+try
+    vwap = loadresults('VWAP','..\results');
+catch
+    mst = selectAndFilterTrades(OPT_EDGES_BAD_PRICES);
+    if isempty(OPT_EDGES_BAD_PRICES)
+        mst = mst(:, {'File','Id','Permno','Date','Isbadday','Isfewobs'});
+    else
+        mst = mst(:, {'File','Id','Permno','Date','MedPrice', 'Isbadday','Isfewobs'});
+    end
+    edges = [93000,100000; 120000,123000; 123000,130000; 153000,160000];
+    vwap  = Analyze('VWAP',[],mst,[],[], struct('edgesVWAP',edges,'edges',OPT_EDGES_BAD_PRICES));
+end
+[idx,pos] = ismembIdDate(master.mst.Permno, master.mst.Date, vwap.Permno, vwap.Date);
+vwap      = vwap(pos(idx),:);
 
 % Common shares
-shrcd      = loadresults('shrcd','../results');
+shrcd = loadresults('shrcd','../results');
+
 idx        = iscommonshare(master.mst, shrcd);
 master.mst = master.mst(idx,:);
-idx        = iscommonshare(price_fl, shrcd);
-price_fl   = price_fl(idx,:);
+
+idx      = iscommonshare(price_fl, shrcd);
+price_fl = price_fl(idx,:);
+
+idx  = iscommonshare(vwap, shrcd);
+vwap = vwap(idx,:);
 
 save('results\master.mat', 'master')
 save('results\price_fl.mat','price_fl')
+save('results\vwap.mat','vwap')
 
 % Capitalizations
 cap         = getMktCap(master.mst.Permno,[],false,false,1);
