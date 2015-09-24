4e43310065a0a2f11b5488eb1199d3e45be31b60
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-09-24 20:57:03 +0100

    speedup

--- utils/selecttrades.m
+++ utils/selecttrades.m
@@ -24,5 +24,6 @@ function idx = selecttrades(data)
 % - anomalous sale conditions, i.e. all but 'E' (69) Automatic Execution, 'F' (70) Intermarket Sweep Order, ' ' (32) No Sale Condition required or '@' (64) Regular Trade
 % - 0 prices
 % - size > 0
-idx = (data.G127_Correction(:,1) ~= 0 & data.G127_Correction(:,1) ~= 40) | data.G127_Correction(:,2) ~= 0 | ~all(data.Condition == ' ' | data.Condition == 'E' | data.Condition == 'F' | data.Condition == '@', 2) | data.Price == 0 | data.Volume <= 0;
+% idx = (data.G127_Correction(:,1) ~= 0 & data.G127_Correction(:,1) ~= 40) | data.G127_Correction(:,2) ~= 0 | ~all(data.Condition == ' ' | data.Condition == 'E' | data.Condition == 'F' | data.Condition == '@', 2) | data.Price == 0 | data.Volume <= 0;
+idx = all(bsxfun(@ne, data.G127_Correction(:,1), [0,40]),2) | data.G127_Correction(:,2) ~= 0 | (all(bsxfun(@ne, data.Condition(:,1), ' EF@'),2) | all(bsxfun(@ne, data.Condition(:,2), ' EF@'),2)) | data.Price == 0 | data.Volume <= 0;
 end
