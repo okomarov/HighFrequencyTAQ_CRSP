c6c5262fadebddd9aa9eeaba9130c064ff6cfeb7
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-10-19 21:52:40 +0100

    cache2cell for parfor

---
+++ utils/cache2cell.m
@@ -0,0 +1,14 @@
+function [cached,unGroups] = cache2cell(data, groupvar)
+% [cached,unGroups] = cache2cell(data, groupvar)
+%
+% NOTE: it sorts data according to groupvar and operates along rows
+
+if size(data,1) ~= numel(groupvar)
+    error('cache2cell:sizeMismatch','DATA must have as many rows as elements in the GROUPVAR.')
+end
+[groupvar,isort]  = sort(groupvar);
+data              = data(isort,:);
+[unGroups,~,subs] = unique(groupvar);
+nrows             = accumarray(subs,1);
+cached            = mat2cell(data,nrows,size(data,2));
+end
