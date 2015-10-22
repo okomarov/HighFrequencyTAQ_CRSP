ac647b4eb0d841fc82498f5ec3e22b47a7d345cf
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-10-22 14:04:11 +0100

    getMktCap complete change in API

--- utils/getMktCap.m
+++ utils/getMktCap.m
@@ -1,9 +1,5 @@
-function cap = getMktCap(permno, date, iscommon,issp500, ispanel, lag)
-if nargin < 1 || isempty(permno),   permno   = [];      end
-if nargin < 2 || isempty(date),     date     = [];      end
-if nargin < 3 || isempty(iscommon), iscommon = true;    end
-if nargin < 4 || isempty(issp500),  issp500  = false;   end
-if nargin < 5 || isempty(ispanel),  ispanel  = false;   end
+function cap = getMktCap(tb, lag, ispanel)
+if nargin < 3 || isempty(ispanel),  ispanel = false;   end
 
 try
     cap = loadresults('mktcap');
@@ -11,7 +7,7 @@ catch
     cap = loadresults('mktcap','..\results');
 end
 
-if nargin == 6 && lag > 0
+if nargin >= 2 && lag > 0
     % Ensure it is sorted by id-date, i.e. some date changes correspond 
     % to same permno, we need to sort
     idx = diff(cap.Date) == 0;
@@ -26,29 +22,11 @@ if nargin == 6 && lag > 0
 end
 
 % Filter by permno, date
-idx = true(size(cap,1),1);
-if ~isempty(permno) && ~isempty(date)
-    idx = idx & ismembIdDate(cap.Permno,cap.Date,permno,date);
-else
-    if ~isempty(permno)
-        idx = idx & ismember(cap.Permno,permno);
-    end
-    if ~isempty(date)
-        idx = idx & ismember(cap.Date,date);
-    end
-end
-
-% Common share and sp500 constituents
-if iscommon
-    idx = idx & iscommonshare(cap);
-end
-if issp500
-    idx = idx & issp500member(cap);
-end
-cap = cap(idx,:);
+[idx,pos] = ismembIdDate(tb.Permno, tb.Date, cap.Permno, cap.Date);
+tb.Cap(idx,1) = cap.Cap(pos(idx));
 
 % Unstack
 if ispanel
-    cap = sortrows(unstack(cap,'Cap','Permno'),'Date');
+    cap = sortrows(unstack(tb(:,{'Cap','Permno','Date'}),'Cap','Permno'),'Date');
 end
 end
