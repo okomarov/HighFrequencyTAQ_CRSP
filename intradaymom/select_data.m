7cf85b76d0a2cf36dc30624e0b4eee79bb5332d4
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-10-22 14:05:24 +0100

    rename master.mst to master

--- intradaymom/select_data.m
+++ intradaymom/select_data.m
@@ -6,15 +6,16 @@ OPT_LAGDAY           = 1;
 % Index data
 datapath = '..\data\TAQ\sampled\5min\nobad';
 master   = load(fullfile(datapath,'master'),'-mat');
+master   = sortrows(master.mst,{'Permno','Date'});
 
 % Common shares
-idx        = iscommonshare(master.mst);
-master.mst = master.mst(idx,:);
+idx    = iscommonshare(master);
+master = master(idx,:);
 
 % Has mkt cap
-cap        = loadresults('mktcap','..\results');
-idx        = ismembIdDate(master.mst.Permno, master.mst.Date, cap.Permno, cap.Date);
-master.mst = master.mst(idx,:);
+cap    = loadresults('mktcap','..\results');
+idx    = ismembIdDate(master.Permno, master.Date, cap.Permno, cap.Date);
+master = master(idx,:);
 
 % Sample first and last price
 try
@@ -28,7 +29,7 @@ catch
     end
     price_fl = Analyze('sampleFirstLast',[],mst,[],[], struct('edges',OPT_EDGES_BAD_PRICES));
 end
-[idx,pos] = ismembIdDate(master.mst.Permno, master.mst.Date, price_fl.Permno, price_fl.Date);
+[idx,pos] = ismembIdDate(master.Permno, master.Date, price_fl.Permno, price_fl.Date);
 price_fl  = price_fl(pos(idx),:);
 
 % Sample VWAP
@@ -44,12 +45,12 @@ catch
     edges = [93000,100000; 120000,123000; 123000,130000; 153000,160000];
     vwap  = Analyze('VWAP',[],mst,[],[], struct('edgesVWAP',edges,'edges',OPT_EDGES_BAD_PRICES));
 end
-[idx,pos] = ismembIdDate(master.mst.Permno, master.mst.Date, vwap.Permno, vwap.Date);
+[idx,pos] = ismembIdDate(master.Permno, master.Date, vwap.Permno, vwap.Date);
 vwap      = vwap(pos(idx),:);
 
 % CRSP 
 crsp      = loadresults('dsfquery','../results');
-[idx,pos] = ismembIdDate(master.mst.Permno, master.mst.Date, crsp.Permno, crsp.Date);
+[idx,pos] = ismembIdDate(master.Permno, master.Date, crsp.Permno, crsp.Date);
 crsp      = crsp(pos(idx),:);
 
 save('results\master.mat', 'master')
@@ -58,21 +59,21 @@ save('results\vwap.mat','vwap')
 save('results\dsfquery.mat','crsp')
 
 % Capitalizations
-cap = sortrows(unstack(cap,'Cap','Permno'),'Date');
-cap = struct('Permnos', {getVariableNames(cap(:,2:end))}, ...
+cap              = sortrows(unstack(cap,'Cap','Permno'),'Date');
+cap              = struct('Permnos', {getVariableNames(cap(:,2:end))}, ...
     'Dates', cap{:,1},...
     'Data', cap{:,2:end});
 % Date intersection
-idx         = ismember(cap.Dates, unique(master.mst.Date));
-cap.Dates   = cap.Dates(idx);
-cap.Data    = cap.Data(idx,:);
+idx              = ismember(cap.Dates, unique(master.Date));
+cap.Dates        = cap.Dates(idx);
+cap.Data         = cap.Data(idx,:);
 % Permno expansion
-xpermnos    = matlab.internal.table.numberedNames('x',unique(master.mst.Permno),false);
-[idx,pos]   = ismember(cap.Permnos, xpermnos);
-data        = NaN(numel(cap.Dates), numel(xpermnos));
+xpermnos         = matlab.internal.table.numberedNames('x',unique(master.Permno),false);
+[idx,pos]        = ismember(cap.Permnos, xpermnos);
+data             = NaN(numel(cap.Dates), numel(xpermnos));
 data(:,pos(idx)) = cap.Data(:,idx);
-cap.Data    = data;
-cap.Permnos = xpermnos;
+cap.Data         = data;
+cap.Permnos      = xpermnos;
 
 save('results\cap.mat','cap')
 
@@ -80,5 +81,5 @@ save('results\cap.mat','cap')
 bpoints = importFrenchData('ME_Breakpoints_TXT.zip','..\results');
 
 % FF 49 industry codes
-industry = getFF49IndustryCodes(master.mst);
+industry = getFF49IndustryCodes(master);
 save('results\FF49.mat','industry')
