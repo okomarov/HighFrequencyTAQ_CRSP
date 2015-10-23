c80c71d0733ad115c27f5e3d59f4745f8e6248fd
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-10-23 20:14:12 +0100

    allow to query single permno

--- utils/getMktCap.m
+++ utils/getMktCap.m
@@ -1,4 +1,4 @@
-function cap = getMktCap(tb, lag, ispanel)
+function tb = getMktCap(tb, lag, ispanel)
 if nargin < 3 || isempty(ispanel),  ispanel = false;   end
 
 try
@@ -22,11 +22,15 @@ if nargin >= 2 && lag > 0
 end
 
 % Filter by permno, date
-[idx,pos] = ismembIdDate(tb.Permno, tb.Date, cap.Permno, cap.Date);
-tb.Cap(idx,1) = cap.Cap(pos(idx));
+if isnumeric(tb)
+    tb = cap(ismember(cap.Permno, tb),:);
+else
+    [idx,pos]     = ismembIdDate(tb.Permno, tb.Date, cap.Permno, cap.Date);
+    tb.Cap(idx,1) = cap.Cap(pos(idx));
+end
 
 % Unstack
 if ispanel
-    cap = sortrows(unstack(tb(:,{'Cap','Permno','Date'}),'Cap','Permno'),'Date');
+    tb = sortrows(unstack(tb(:,{'Cap','Permno','Date'}),'Cap','Permno'),'Date');
 end
 end
