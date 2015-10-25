a059095b87081888894b7bee8ce0c555cbbe73fc
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-10-25 20:36:45 +0000

    check old/new ff49 classification API

--- intradaymom/checks.m
+++ intradaymom/checks.m
@@ -15,10 +15,10 @@ taq       = taq(ib,:);
 % Filter out outliers
 taq.TAQret   = taq.LastPrice./taq.FirstPrice-1;
 iout         = taq.TAQret          > OPT_OUTLIERS_THRESHOLD |...
-               1./(taq.TAQret+1)-1 > OPT_OUTLIERS_THRESHOLD;
-taq(iout,:)  = [];            
+    1./(taq.TAQret+1)-1 > OPT_OUTLIERS_THRESHOLD;
+taq(iout,:)  = [];
 crsp(iout,:) = [];
-            
+
 % Comparison table
 cmp         = [crsp(:,{'Date','Permno','Openprc'}) taq(:,'FirstPrice'), ...
     crsp(:,{'Bid','Ask','Prc'}),taq(:,{'LastPrice','TAQret'})];
@@ -27,3 +27,29 @@ cmp.CRSPret = cmp.Prc./cmp.Openprc-1;
 retdiff = abs(cmp.CRSPret - cmp.TAQret);
 idx     = retdiff > eps*1e12;
 boxplot(retdiff(idx))
+%% Check new/old ff49
+load('D:\TAQ\HF\intradaymom\results\bck\FF49.mat')
+
+taq  = loadresults('price_fl');
+ff49 = getFF49IndustryCodes(taq,1);
+ff49 = struct('Permno', xstr2num(getVariableNames(ff49(:,2:end))), ...
+    'Dates', ff49{:,1},...
+    'Data', ff49{:,2:end});
+
+% Intersect permno
+[~,ia,ib] = intersect(industry.Permno, ff49.Permno);
+industry.Data = industry.Data(:,ia);
+industry.Permno = industry.Permno(ia);
+ff49.Data = ff49.Data(:,ib);
+ff49.Permno = ff49.Permno(ib);
+
+% intersect date
+[~,ia,ib] = intersect(industry.Date, ff49.Dates);
+industry.Data = industry.Data(ia,:);
+industry.Date = industry.Date(ia);
+ff49.Data = ff49.Data(ib,:);
+ff49.Dates = ff49.Dates(ib);
+
+[r,c] = find(ff49.Data ~= industry.Data & ff49.Data ~= 0)
+
+
