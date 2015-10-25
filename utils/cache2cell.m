5d17f532907df2ff3562efc8d1b48c65cdedb14c
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-10-25 19:52:59 +0000

    sort only if necessary

--- utils/cache2cell.m
+++ utils/cache2cell.m
@@ -6,8 +6,10 @@ function [cached,unGroups] = cache2cell(data, groupvar)
 if size(data,1) ~= numel(groupvar)
     error('cache2cell:sizeMismatch','DATA must have as many rows as elements in the GROUPVAR.')
 end
-[groupvar,isort]  = sort(groupvar);
-data              = data(isort,:);
+if ~issorted(groupvar)
+    [groupvar,isort]  = sort(groupvar);
+    data              = data(isort,:);
+end
 [unGroups,~,subs] = unique(groupvar);
 nrows             = accumarray(subs,1);
 cached            = mat2cell(data,nrows,size(data,2));
