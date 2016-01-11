053167924423bfe89083a8a94ed2e756e358a099
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-11 22:13:47 +0000

    add betas and hpr

--- hfandlow/make_signals.m
+++ hfandlow/make_signals.m
@@ -1,9 +1,10 @@
-function signals = make_signals(ret,date,factors)
+function [signals,hpr] = make_signals(ret,date,factors)
 [unDt,~,midx] = unique(date/100);
 nmonths       = numel(unDt);
 nseries       = size(ret,2);
 
-signals = NaN(nmonths, nseries,3);
+signals = NaN(nmonths, nseries,4);
+hpr     = NaN(nmonths, nseries);
 for ii = 1:nmonths
     imonth = midx == ii;
     nobs   = nnz(imonth);
@@ -27,5 +28,39 @@ for ii = 1:nmonths
     % Skewness
     signals(ii,nonans,2) = skewness(r);
     signals(ii,nonans,3) = sqrt(nobs) * sum(r.^3) ./ sum(r.*r).^1.5;
+end
+
+for ii = 12:nmonths
+    iyear = ismember(midx, ii-12+1:ii);
+    nobs  = nnz(iyear);
+
+    % Month of all non-nan returns
+    r      = ret(iyear,:);
+    inan   = isnan(r);
+    nonans = sum(~inan) >= 200;
+    ngood  = nnz(nonans);
+    inan   = inan(:,nonans);
+    r      = r(:,nonans);
+
+    % Betas
+    Y     = bsxfun(@minus,r, factors.RF(iyear)/100);
+    X     = [ones(nobs,1), factors.MktMinusRF(iyear)/100];
+    coeff = NaN(2,ngood);
+
+    for jj = 1:ngood
+        idx         = ~inan(:,jj);
+        coeff(:,jj) = X(idx,:)\Y(idx,jj);
+    end
+    signals(ii,nonans,4) = coeff(2,:);
+end
+
+% Holding period return
+for ii = 1:nmonths
+    imonth            = midx == ii;
+    r                 = ret(imonth,:);
+    inan              = isnan(r);
+    r(inan)           = 0;
+    hpr(ii,:)         = prod(1+r)-1;
+    hpr(ii,all(inan)) = NaN;
 end
 end
