0a6cd268033b0992dc0e10a3ddafc7d04b3e89d6
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-12 20:04:34 +0000

    simplify overnight intraday and close to close return estimation

--- hfandlow/utils/estimateOvernightretIntraday.m
+++ hfandlow/utils/estimateOvernightretIntraday.m
@@ -4,30 +4,17 @@ function res = estimateOvernightretIntraday
 
 
 % Load Permno - Date pairs
-mst = loadresults('masterPermno');
+mst = loadresults('masterPermno','..\results');
+mst = addPermno(mst);
 
-% Load overnight
-try
-    res = loadresults('return_intraday');
-catch
-    % Median price
-    cached = loadresults('medianprice');
-    
-    % Add File
-    path2data   = '.\data\TAQ\';
-    master      = load(fullfile(path2data,'master'),'-mat');
-    [~,pos]     = ismembIdDate(cached.Id, cached.Date, master.mst.Id, master.mst.Date);
-    cached.File = master.mst.File(pos);
-    clear master
-    
-    res = Analyze('return_intraday',[],cached,path2data);
-end
-[~,pos]   = ismembIdDate(mst.Id, mst.Date, res.Id, res.Date);
-mst.RetOC = res.RetOC(pos);
-clear res
+% Open-to-close returns
+fl        = loadresults('sampleFirstLast','..\results');
+retOC     = fl.LastPrice./fl.FirstPrice-1;
+[~,pos]   = ismembIdDate(mst.Id, mst.Date, fl.Id, fl.Date);
+mst.RetOC = retOC(pos);
 
 % Load dsfquery
-dsfquery = loadresults('dsfquery');
+dsf = loadresults('dsfquery','..\results');
 
 % % Winsorize returns at 0.1 and 99.9%
 % ptiles = prctile(dsfquery.Ret,[0.1,99.9]);
@@ -37,14 +24,13 @@ dsfquery = loadresults('dsfquery');
 % dsfquery.Ret(dsfquery.Ret < ptiles(1)) = ptiles(1);
 % dsfquery.Ret(dsfquery.Ret > ptiles(2)) = ptiles(2);
 
-% Add RetCC from dsfquery
-[idx,pos]      = ismembIdDate(mst.Permno, mst.Date, dsfquery.Permno, dsfquery.Date);
+% Close-to-close return
+[idx,pos]      = ismembIdDate(mst.Permno, mst.Date, dsf.Permno, dsf.Date);
 mst.RetCC      = NaN(size(mst.Permno));
-mst.RetCC(idx) = dsfquery.Ret(pos(idx));
-clear dsfquery
+mst.RetCC(idx) = dsf.Ret(pos(idx));
 
 % Overnight return
-mst.RetCO = log((1 + mst.RetCC)./(1 + mst.RetOC )); 
+mst.RetCO = log((1 + mst.RetCC)./(1 + mst.RetOC ));
 
 res      = mst;
 filename = sprintf('%s_%s.mat',datestr(now,'yyyymmdd_HHMM'),'return_intraday_overnight');
