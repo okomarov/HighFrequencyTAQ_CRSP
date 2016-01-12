5cd86450f2a6c36bba2fb3a05c4b8b5324fb68e8
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-12 20:12:55 +0000

    cosmetic

--- hfandlow/utils/getSpy.m
+++ hfandlow/utils/getSpy.m
@@ -10,30 +10,29 @@ if nargin < 3 || isempty(to),   to   = inf; end
 if nargin < 1 || isempty(freq), freq = 5; end
 name = sprintf('spy%dm.mat',freq);
 
-% Unsampled 
+% Unsampled
 if isinf(freq)
     spy = getTaqData('symbol','SPY',from,to);
-    return
-end
 
-% Load sampled
-try
-    spy = loadresults(name);
-% Sample    
-catch
-    fprintf('%s: sampling SPY at %d min.\n', mfilename, freq)
-    opt.grid = (9.5/24:freq/(60*24):16/24)';
-    writeto  = '.\results';
-    
-    % Sample at x min
-    [spy, filename] = Analyze('sampleSpy',[],[],[],[],opt);
-    
-    % Rename to append the sampling frequency
-    newfullname = fullfile(writeto, name);
-    movefile(fullfile(writeto,filename), newfullname);
+% Sampled
+else
+    try
+        spy = loadresults(name);
+    catch
+        % Sample
+        fprintf('%s: sampling SPY at %d min.\n', mfilename, freq)
+        opt.grid = (9.5/24:freq/(60*24):16/24)';
+        writeto  = '.\results';
+        
+        % Sample at x min
+        [spy, filename] = Analyze('sampleSpy',[],[],[],[],opt);
+        
+        % Rename to append the sampling frequency
+        newfullname = fullfile(writeto, name);
+        movefile(fullfile(writeto,filename), newfullname);
+    end
+    % Filter dates
+    idx = in(serial2yyyymmdd(spy.Datetime),[from, to]);
+    spy = spy(idx,:);
 end
-
-% Filter dates
-idx = in(serial2yyyymmdd(spy.Datetime),[from, to]);
-spy = spy(idx,:);
 end
