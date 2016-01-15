fe2255264dfc9a998302f48776e10e686f010508
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-15 20:57:38 +0000

    adding overnight returns

---
+++ utils/addOvernightRet.m
@@ -0,0 +1,27 @@
+function tb = addOvernightRet(tb, reton)
+% Returns are treated as log returns
+
+% See also: estimateOverIntraCloseRet
+
+if nargin < 2
+    reton = loadresults('return_intraday_overnight');
+end
+
+try
+    tb.Date(1);
+catch
+    tb.Date = serial2yyyymmdd(tb.Datetime);
+end
+
+% Find where overnight should be positioned
+iDateChange   = [true; diff(int32(tb.Date)) ~= 0];
+iPermnoChange = [true; diff(int32(tb.Permno)) ~= 0];
+posOn         = find(iPermnoChange | iDateChange);
+
+% Check if has already a return
+iadd      = ~isnan(tb.Ret(posOn));
+[idx,pos] = ismembIdDate(tb.Permno(posOn), tb.Date(posOn),reton.Permno,reton.Date);
+
+tb.Ret(posOn(idx &  iadd)) = tb.Ret(idx & iadd) + reton.RetCO(pos(idx & iadd));
+tb.Ret(posOn(idx & ~iadd)) = reton.RetCO(pos(idx & ~iadd));
+end
