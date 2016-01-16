7ef83a3a3f7deefec27dae40f64628dd7758ba83
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-16 20:49:19 +0000

    alpha and realized skew signals

--- hfandlow/make_signals_HF.m
+++ hfandlow/make_signals_HF.m
@@ -1,95 +1,99 @@
-function [signals,hpr,rf] = make_signals_HF(permno,date,factors)
-
-reton = loadresults('return_intraday_overnight');
+function signals = make_signals_HF(permno,date,master,reton,factors,skew,beta)
 
 [unDt,~,midx] = unique(date/100);
 nmonths       = numel(unDt);
 nseries       = numel(permno);
+signals       = NaN(nmonths, nseries,3);
 
-signals = NaN(nmonths, nseries,4);
-hpr     = NaN(nmonths, nseries);
-rf      = NaN(nmonths, 1);
+% Alpha
+signals(:,:,1) = estimateAlpha(permno,date,factors,master,reton);
 
-% Get market return
-mkt             = getSpy(5, min(date),max(date));
-mkt.Permno(:,1) = 84398;
-mkt             = price2ret(mkt);
-mkt             = addOvernightRet(mkt,reton);
+% Skewness
+for ii = 1:nmonths
+    imonth          = midx == ii;
+    signals(ii,:,2) = nanmean(skew(imonth,:));
+end
 
-% Get rf
+% % Betas
+% for ii = 12:nmonths
+%     iyear = ismember(midx, ii-12+1:ii);
+%     nobs  = nnz(iyear);
+%
+%     signals(ii,nonans,4) = coeff(2,:);
+% end
+end
 
+function signals = estimateAlpha(permno,date,factors,master,reton)
+
+unDt    = unique(date/100);
+nmonths = numel(unDt);
+nseries = numel(permno);
+signals = NaN(nmonths, nseries);
 
 for ii = 1:nmonths
-    [ret, permnoFound] = getHighFreqRet(unDt(ii),permno,reton);
+    % Get data
+    [ret, permnoFound, dt] = getHighFreqRet(unDt(ii),reton, master);
 
-    % Month of all non-nan returns
-    r      = data(imonth,:);
-    nonans = all(~isnan(r));
-    ngood  = nnz(nonans);
-    r      = r(:,nonans);
+    % make sure dates are same
+    idx = ismember(dt,date);
+    ret = ret(idx,:);
+    dt  = dt(idx);
+
+    % Get mkt
+    imkt        = permnoFound == 84398;
+    mkt         = ret(:, imkt);
+    ret         = ret(:,~imkt);
+    permnoFound = permnoFound(~imkt);
+
+    % Expand rf
+    [~,~,subs] = unique(dt);
+    idx        = ismember(factors.Date, unique(dt));
+    rf         = RunLength(factors.RF(idx)/100, accumarray(subs,1));
 
     % Alpha
-    Y     = bsxfun(@minus,r, factors.RF(imonth)/100);
-    X     = [ones(nobs,1), factors.MktMinusRF(imonth)/100];
-    coeff = NaN(2,ngood);
+    Y = bsxfun(@minus,ret,rf);
+    X = [ones(numel(dt),1), mkt-rf];
 
-    for jj = 1:ngood
-        coeff(:,jj) = X\Y(:,jj);
+    % At least some obs
+    igood = sum(~bsxfun(@or, isnan(mkt), isnan(Y))) > 10;
+
+    coeff = NaN(2,numel(permnoFound));
+    for jj = 1:numel(permnoFound)
+        if igood(jj)
+            coeff(:,jj) = regress(Y(:,jj),X);
+        end
     end
-    signals(ii,nonans,1) = coeff(1,:);
+    [~,pos]         = ismember(permnoFound, permno);
+    signals(ii,pos) = coeff(1,:);
 
-    % Skewness
-    signals(ii,nonans,2) = skewness(r);
-    signals(ii,nonans,3) = sqrt(nobs) * sum(r.^3) ./ sum(r.*r).^1.5;
-end
-
-% Betas
-for ii = 12:nmonths
-    iyear = ismember(midx, ii-12+1:ii);
-    nobs  = nnz(iyear);
-
-    % Month of all non-nan returns
-    r      = data(iyear,:);
-    inan   = isnan(r);
-    nonans = sum(~inan) >= 200;
-    ngood  = nnz(nonans);
-    inan   = inan(:,nonans);
-    r      = r(:,nonans);
-
-    % Betas
-    Y     = bsxfun(@minus,r, factors.RF(iyear)/100);
-    X     = [ones(nobs,1), factors.MktMinusRF(iyear)/100];
-    coeff = NaN(2,ngood);
-
-    for jj = 1:ngood
-        idx         = ~inan(:,jj);
-        coeff(:,jj) = X(idx,:)\Y(idx,jj);
-    end
-    signals(ii,nonans,4) = coeff(2,:);
+    %     % Skewness
+    %     signals(ii,nonans,2) = skewness(r);
+    %     signals(ii,nonans,3) = sqrt(nobs) * sum(r.^3) ./ sum(r.*r).^1.5;
 end
 end
 
-function [data, permnoFound] = getHighFreqRet(month, permno, reton)
-    % Get prices
-    from = month*100+1;
-    to   = month*100+31;
-    data = getTaqData('permno',permno,from,to,[],'..\data\TAQ\sampled\5min\nobad_vw');
+function [data, permnoFound, dates] = getHighFreqRet(month, reton, master)
+% Get prices
+from = month*100+1;
+to   = month*100+31;
+data = getTaqData('permno',[],from,to,[],'..\data\TAQ\sampled\5min\nobad_vw',master,false);
 
-    % Add returns
-    data = price2ret(data);
-    data = addOvernightRet(data,reton);
+% Add returns
+data = price2ret(data);
+data = addOvernightRet(data,reton(in(reton.Date,[from,to]),:));
 
-    % Unstack
-    data.Date   = serial2yyyymmdd(data.Datetime);
-    data.Time   = serial2hhmmss(data.Datetime);
-    data        = sortrows(unstack(data(:,{'Date','Time','Permno','Ret'}),'Ret','Permno'),{'Date','Time'});
-    permnoFound = xstr2num(data.Properties.VariableNames(3:end));
-    data        = data{:,3:end};
+% Unstack
+data.Date   = serial2yyyymmdd(data.Datetime);
+data.Time   = serial2hhmmss(data.Datetime);
+data        = sortrows(unstack(data(:,{'Date','Time','Permno','Ret'}),'Ret','Permno'),{'Date','Time'});
+permnoFound = xstr2num(data.Properties.VariableNames(3:end));
+dates       = data.Date;
+data        = data{:,3:end};
 end
 
 function data = price2ret(data)
-    data.Ret        = [NaN; diff(log(data.Price))];
-    ion             = [true; diff(fix(data.Datetime)) ~= 0] |...
-                      [true; diff(data.Permno) ~= 0];
-    data.Ret(ion,1) = NaN;
+data.Ret        = [NaN; diff(log(data.Price))];
+ion             = [true; diff(fix(data.Datetime)) ~= 0] |...
+                  [true; diff(data.Permno) ~= 0];
+data.Ret(ion,1) = NaN;
 end
