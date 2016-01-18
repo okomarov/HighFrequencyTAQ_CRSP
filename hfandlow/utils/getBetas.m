6da2d99c6ebc7bd43b5de3b1e0700e4bd2aa7051
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-18 19:34:30 +0000

    drop lookback input

--- hfandlow/utils/getBetas.m
+++ hfandlow/utils/getBetas.m
@@ -1,17 +1,16 @@
-function [betas, permnos] = getBetas(lookback, freq, useon, useproxy, issp, iscs, keeplong)
+function [betas, permnos] = getBetas(freq, useon, useproxy, issp, iscs, keeplong)
 % GETBETAS Loads betas, applies sp500 and/or common shares filers, unstacks
 %
 %   getBetas(lookback, freq, useon, useproxy, issp, iscs, keeplong)
 %
 %   NOTE: Does NOT fill in between NaNs
 
-if nargin < 1 || isempty(lookback), lookback = 1;       end
-if nargin < 2 || isempty(freq),     freq     = 5;       end
-if nargin < 3 || isempty(useon),    useon    = true;    end
-if nargin < 4 || isempty(useproxy), useproxy = false;   end
-if nargin < 5 || isempty(issp),     issp     = true;    end
-if nargin < 6 || isempty(iscs),     iscs     = true;    end
-if nargin < 7 || isempty(keeplong), keeplong = false;   end
+if nargin < 1 || isempty(freq),     freq     = 5;       end
+if nargin < 2 || isempty(useon),    useon    = true;    end
+if nargin < 3 || isempty(useproxy), useproxy = false;   end
+if nargin < 4 || isempty(issp),     issp     = true;    end
+if nargin < 5 || isempty(iscs),     iscs     = true;    end
+if nargin < 6 || isempty(keeplong), keeplong = false;   end
 
 % Load all betas
 betas = estimateBetaComponents(freq, useon, useproxy);
@@ -39,7 +38,7 @@ if ~keeplong
     betas = unstack(betas(:,{'Date','Permno','Beta'}), 'Beta','Permno');
     betas = sortrows(betas,'Date');
 end
-    
+
 % Convert to double
 betas = tbextend.varfun(@double, betas, 'RenameVariables', false);
 end
