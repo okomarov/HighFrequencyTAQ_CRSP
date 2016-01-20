dbfd60357f6e2b494952074255ba33b164a7970b
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-20 13:54:22 +0000

    move data unstacking into sample.m

--- hfandlow/main.m
+++ hfandlow/main.m
@@ -5,50 +5,17 @@ OPT_LAG    = 1;
 OPT_PTF_UN = 10;
 
 %% Data
+load('results\alldata')
 master = loadresults('master');
+reton  = loadresults('reton');
 
-myunstack = @(tb,vname) sortrows(unstack(tb(:,{'Permno','Date',vname}),vname,'Permno'),'Date');
-
-% Returns
-dsf    = loadresults('dsf');
-ret    = myunstack(dsf,'Ret');
-date   = ret.Date;
-permno = ret.Properties.VariableNames(2:end);
-ret    = double(ret{:,2:end});
-
-% End-of-Month ismicro
-dsf.IsMicro = isMicrocap(dsf,'Prc');
-isMicro     = myunstack(dsf,'IsMicro');
-[~,pos]     = unique(isMicro.Date/100,'last');
-isMicro     = isMicro{pos,2:end};
-
-% Mkt cap
-dsf     = getMktCap(dsf);
-cap     = myunstack(dsf,'Cap');
-[~,pos] = unique(cap.Date/100,'last');
-cap     = cap{pos,2:end};
-
-% Realized skewness
-rskew = loadresults('skew');
-num   = myunstack(rskew,'Num');
-den   = myunstack(rskew,'Den');
-rskew = num{:,2:end}./den{:,2:end};
-clear den num
-
-% Overnight return
-reton = loadresults('reton');
-
-% Beta components
-beta              = loadresults('beta5minon');
-num               = myunstack(beta,'Num');
-den               = myunstack(beta,'Den');
-beta              = cat(3,num{:,2:end},den{:,2:end});
-beta(isinf(beta)) = NaN; % permno 46288 on 19931008 is delisted with close-to-close return of -100%
-clear den num
-
-% Factors
-ff = loadresults('F-F_Research_Data_5_Factors_2x3_daily_TXT');
-ff = ff(ismember(ff.Date, unique(dsf.Date)),:);
+% % Things in alldata, already unstacked
+% dsf         = loadresults('dsf');
+% dsf.IsMicro = isMicrocap(dsf,'Prc');
+% dsf         = getMktCap(dsf);
+% rskew       = loadresults('skew');
+% beta        = loadresults('beta5minon');
+% ff          = loadresults('F-F_Research_Data_5_Factors_2x3_daily_TXT');
 %% Signals
 
 % Low freqeuncy signals
