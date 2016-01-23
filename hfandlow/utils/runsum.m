1dd9933c3b7169e5b2814b1e9bf8245552601469
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-23 21:48:35 +0000

    remove hfandlow and intradaymom subfolders

--- hfandlow/utils/runsum.m
+++
@@ -1,6 +0,0 @@
-function rs = runsum(lookback, data)
-rs        = NaN(size(data));
-nonan     = ~isnan(data);
-rs(nonan) = filter(ones(lookback,1), 1, data(nonan), NaN(lookback-1,1));
-rs        = {rs};
-end
