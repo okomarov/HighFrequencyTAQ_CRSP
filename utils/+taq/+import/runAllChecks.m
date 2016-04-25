ba9c9b2fed2e743d056460ca34c6b457e7ad46e0
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-04-25 18:17:11 +0100

    price series comparison

--- utils/+taq/+import/runAllChecks.m
+++ utils/+taq/+import/runAllChecks.m
@@ -1,12 +1,12 @@
 function runAllChecks(matfolder,testfolder)
-% Day spillover
+%% Day spillover
 prob = checkDaySpillover(matfolder);
 if ~isempty(prob)
     disp(prob)
     error('There are spillovers of daily data across multiple files.')
 end
 
-% Check master from-to continuity
+%% Master from-to continuity
 dd         = dir(fullfile(matfolder,'*.idx'));
 indexnames = {dd.name};
 for ii = 1:numel(indexnames)
@@ -15,4 +15,39 @@ for ii = 1:numel(indexnames)
         error('File %d has gaps between To and the next From.',ii)
     end
 end
+
+%% Compare price series
+FMT    = '%s%u32%u8:%u8:%u8%f32%u32%u16%u16%s%c';
+OPTS   = {'Delimiter',',','HeaderLines',1};
+VNAMES = {'Symbol','Date','HH','MM','SS','Price','Size','G127','Corr','Cond','Ex'};
+
+folders = dir(fullfile(testfolder));
+folders = {folders.name};
+folders = fullfile(testfolder, folders(~ismember(folders, {'.','..'})));
+for f = folders
+    zipfiles = dir(fullfile(f{1},'*.zip'));
+    zipfiles = fullfile(f{1},{zipfiles.name});
+
+    for z = zipfiles
+        fname   = unzip(z{1},tempdir());
+        fid     = fopen(fname{1});
+        cleanup = onCleanup(@() myCleanup(fname{1},fid));
+
+        manual = textscan(fid, FMT, OPTS{:});
+        manual = table(manual{:},'VariableNames',VNAMES);
+
+        automated = taq.getTrades('symbol',manual.Symbol{1},[manual.Date(1), manual.Date(end)],matfolder);
+        time      = hhmmssmat2serial(automated.Time);
+        idx       = in(time, hhmmssmat2serial([9 30 0; 16 0 0])');
+
+        if ~isequal(manual.Price, automated.Price(idx))
+            error('There are differences in the price series for "%s".',z{1})
+        end
+    end
+end
+end
+
+function myCleanup(fname, fid)
+fclose(fid);
+delete(fname)
 end
